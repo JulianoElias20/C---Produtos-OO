41e058859143a2f8decc5263d67df8adae7ef355
Juliano Elias <julianorelias94@hotmail.com>
2024-04-25 01:44:53 -0300

    Criado métodos sem encapsulamento

---
+++ ProdutoOOSimples/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutoOOSimples
+{
+    internal class Produto
+    {
+        public string Name;
+        public double Preco;
+        public int Quantidade;
+
+
+        public double ValorTotal()
+        {
+            return Quantidade * Preco;
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, $ {Preco.ToString("F2")}, {Quantidade} unidades, Total: {ValorTotal().ToString("F2")}";
+        }
+    }
+}
---
+++ ProdutoOOSimples/Program.cs
@@ -0,0 +1,42 @@
+﻿using ProdutoOOSimples;
+using System;
+using System.Net.Http.Headers;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do produto:");
+
+            Produto prod = new Produto();
+
+            Console.Write("Nome: ");
+            prod.Name = Console.ReadLine();
+            Console.Write("Preço: ");
+            prod.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: ");
+            prod.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: "+prod.ToString());
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            prod.AdicionarProduto(quantidade);
+            Console.WriteLine("Dados atualizado "+prod.ToString());
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+
+            prod.RemoverProduto(quantidade);
+            Console.WriteLine("Dados atualizado " + prod.ToString());
+
+
+
+
+
+
+        }
+    }
+}
